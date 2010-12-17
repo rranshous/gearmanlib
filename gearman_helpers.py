9538726fc3721f53b1efed44ab97f0d39330bf5d
Robby Ranshous <rranshous@gmail.com>
2010-12-16 22:43:04 -0500

    working toward making convenience functions for farming out

--- gearman_helpers.py
+++ gearman_helpers.py
@@ -1,5 +1,14 @@
 import pickle
 import gearman
+from decorator import decorator
+
+def get_key(function):
+    # for now we are going to go working dir + function name
+    import os
+    working_dir = os.path.basename(os.path.abspath('.'))
+    function_name = function.__name__
+    module_name = function.__module__
+    return '%s_%s' % (working_dir,function_name)
 
 class PickleDataEncoder(gearman.DataEncoder):
     @classmethod
@@ -17,10 +26,28 @@ class PickleGearmanWorker(gearman.GearmanWorker):
     data_encoder = PickleDataEncoder
 
 
-def call_gearman(func_name,*args,**kwargs):
+def call_gearman(key,*args,**kwargs):
     client = PickleGearmanClient(kwargs.get('hosts',[]) + ['127.0.0.1'])
-    r = client.submit_job(func_name,args)
+    del kwargs['hosts']
+    r = client.submit_job(key,(args,kwargs))
     print 'result: %s' % r
     return r.result
 
+def gearmanize(function):
+    def gearmanized(*args,**kwargs):
+        return call_gearman(get_key(function),*args,**kwargs)
+    return gearmanized
 
+@decorator
+def asycable(f,*args,**kwargs):
+    """
+    We are trying to enable farming random functions off to gearman 
+    if there are any workers available
+    """
+    # check and see if we have an async arg
+    if kwargs.get('async',False):
+        pass
+    # if we are async'n than lets try to farm this bitch
+    # out
+    result = call_gearman()
+        
--- lib_wrapper.py
+++ lib_wrapper.py
@@ -9,7 +9,7 @@
 # available
 
 if __name__ == '__main__':
-        
+    from gearman_helpers import get_key
     import os
     print os.path.abspath('.')
     import sys
@@ -21,19 +21,26 @@ if __name__ == '__main__':
     import gearman
     from gearman_helpers import PickleGearmanWorker
     gm_worker = PickleGearmanWorker(['localhost'])
-#gm_worker.set_client_id('your_worker_client_id_name')
+    #gm_worker.set_client_id('your_worker_client_id_name')
     def decode(gearman_worker,gearman_job):
         try:
             data = gearman_job.data
+            # if your a tuple than we want to expand you
             if type(data) is tuple:
-                print 'tuple'
-                return f(*data)
+                # if you are a tuple of a tuple and a dict than
+                # we will also kw expand you
+                if len(data) == 2 and isinstance(data[0],tuple) and \
+                                      isinstance(data[1],dict):
+                    return f(*data[0],**data[1])
+                else:
+                    return f(*data)
             return f(data)
         except Exception, ex:
             print 'exception: %s' % ex
             raise
-    gm_worker.register_task('%s_%s' % (module,func), decode)
+    key = get_key(f)
+    gm_worker.register_task(key, decode)
 
-# Enter our work loop and call gm_worker.after_poll() after each time we timeout/see socket activity
+    # Enter our work loop and call gm_worker.after_poll() after each time we timeout/see socket activity
     print 'working on %s_%s' % (module,func)
     gm_worker.work()
